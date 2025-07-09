b5576c8a2cf2d453c687ac5c4d163b31faa49dcf
guoe2005 <guoe2005@126.com>
2025-07-09 14:30:29 +0800

    cpp

--- CPP/hello.cpp
+++ CPP/hello.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, world" <<endl;
+    cout << "请输入你的名字：\n";
+    string name;
+    cin >> name;
+    cout << "你好，" << name << "！\n";
     return 0;
 }
