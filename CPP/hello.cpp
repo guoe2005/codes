dc1fa7aaae58cdfb701665a67878c71054bbadf5
guoe2005 <guoe2005@126.com>
2025-07-12 10:48:29 +0800

    改变枚举值的基本类型

--- CPP/hello.cpp
+++ CPP/hello.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-   enum class MyEnum {
-     EnumValue1,
-     EnumValue2 = 10,
-     EnumValue3
+  enum class MyEnumLong : unsigned long
+  {
+     EnumValueLong1,
+     EnumValueLong2 = 10,
+     EnumValueLong3
    };
-   if (MyEnum::EnumValue3 == 11) {...}
 }
