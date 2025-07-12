93aec89fe61917b028a2f7e9658a38d65b8ef4f1
guoe2005 <guoe2005@126.com>
2025-07-12 10:53:39 +0800

    结构
    结构允许将一个或多个已有类型封装到一个新的类型中;数据库记录是结构的经
    典示例

--- CPP/hello.cpp
+++ CPP/hello.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-  enum class MyEnumLong : unsigned long
-  {
-     EnumValueLong1,
-     EnumValueLong2 = 10,
-     EnumValueLong3
-   };
+  typedef struct {
+    char firstInitial;
+    char middleInitial;
+    char lastInitial;
+    int salary;
+  } EmployeeT;
 }
