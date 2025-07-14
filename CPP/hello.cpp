c5be01a240b66914d2f3a730874d98bc7b620b7d
guoe2005 <guoe2005@126.com>
2025-07-14 15:27:44 +0800

    1.1.6 条件语句

--- CPP/hello.cpp
+++ CPP/hello.cpp
@@ -3,18 +3,5 @@
 using namespace std;
 int main()
 {
-  // Creae and populate an employee.
-  EmployeeT anEmployee;
-  anEmployee.fistInitial = 'M';
-  anEmployee.middleInitial = 'R';
-  anEmployee.lastInitial = 'G';
-  anEmployee.employeeNumber = 42;
-  anEmployee.salary = 80000;
-
-  cout << "Employee: " << anEmployee.firstInitial <<
-    anEmployee.middleInitial <<
-    anEmployee.lastInitial << endl;
-  cout << "Number: " << anEmployee.employeeNumber << endl;
-  cout << "Salary: $" << anEmployee.salary << endl;
-  return 0;
+  //三种语句
 }
