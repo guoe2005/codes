677bdc5ce8f48b735b8ac2d8763a06d22f39c7d6
guoe2005 <guoe2005@126.com>
2025-10-09 16:04:25 +0800

    java 流转换

--- java/com/demo/StreamFilterDemo.java
+++ java/com/demo/StreamFilterDemo.java
@@ -6,16 +6,16 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class StreamFilterDemo{
-    public static void main (String[] args){
-	Path parent = Paths.get("..");
-	try{
-	    Stream<Path> list = Files.walk(parent);
-	    list.filter((Path p) -> p.toString().endsWith(".java")).
-		forEach(System.out::println);
-	} catch (IOException ex) {
-	    ex.printStackTrace();
-	}
-    }
+    // public static void main (String[] args){
+    // 	Path parent = Paths.get("..");
+    // 	try{
+    // 	    Stream<Path> list = Files.walk(parent);
+    // 	    list.filter((Path p) -> p.toString().endsWith(".java")).
+    // 		forEach(System.out::println);
+    // 	} catch (IOException ex) {
+    // 	    ex.printStackTrace();
+    // 	}
+    // }
 }
 
 
